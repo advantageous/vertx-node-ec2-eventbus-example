ed13f2de4bec1ece751253b7e33b79e415e54d33
Richard Hightower <rick@Richards-MacBook-Pro.local>
2016-01-14 14:45:54 -0800

    Hello World No Event Bus

---
+++ vertx/src/main/java/com/github/vertx/node/example/HelloWorldVerticle.java
@@ -0,0 +1,12 @@
+package com.github.vertx.node.example;
+
+import io.vertx.core.AbstractVerticle;
+
+public class HelloWorldVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+    }
+}
+
