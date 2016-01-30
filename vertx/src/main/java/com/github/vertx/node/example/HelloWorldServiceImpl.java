d32403d422809fa7988f5965568ba0ae477baab3
Richard Hightower <rick@Richards-MacBook-Pro.local>
2016-01-29 16:55:31 -0800

    Setting up ELK done.

--- vertx/src/main/java/com/github/vertx/node/example/HelloWorldServiceImpl.java
+++ vertx/src/main/java/com/github/vertx/node/example/HelloWorldServiceImpl.java
@@ -3,13 +3,19 @@ package com.github.vertx.node.example;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class HelloWorldServiceImpl implements HelloWorldServiceInterface {
+    private final Logger logger = LoggerFactory.getLogger(HelloWorldServiceImpl.class);
+
 
     @Override
     public void hello(final String message, final Handler<AsyncResult<String>> resultHandler) {
 
+
+        logger.info("HelloWorldServiceImpl hello was called {}", message);
         resultHandler.handle(Future.succeededFuture("Hello World! " + message));
 
     }
